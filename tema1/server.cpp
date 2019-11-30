11909e1f96c8f4cfa2344fad50510a7ab6cd200d
George Diaconu <pgn.george@gmail.com>
2019-11-30 19:33:03 +0200

    update tema 1

--- tema1/server.cpp
+++ tema1/server.cpp
@@ -23,18 +23,26 @@ static bool isServerInit = false;
 static std::unordered_map<std::string, int> word_hits_map;
 static char const * last_request = nullptr;
 
-bool init_server();
-bool appendFile(char * cPtrWord);
-response get_response(char const * respStr = "", int hits = -1);
+bool initServer();
+
+bool appendFile(char * word);
+
+// Intoarce o structura response ce contine parametrii dati functiei.
+response getResponse(char const * respStr = "", int hits = -1);
+
+// Verifica daca sirul dat ca argument este un numar, intorcand true si numarul
+// parsat in al doilea parametru, sau false si o valoare necunoscuta in
+// al doilea parametru.
 bool isNumber(char *str, size_t *outNumber);
 
+// Functie RPC prin care clientul transmite ce request face (SEARCH / APPEND)
 char ** start_request_1_svc(request *r, struct svc_req *cl)
 {
     static char const * response = NO;
     last_request = nullptr;
 
     if (!isServerInit) {
-        if (!init_server()) {
+        if (!initServer()) {
             return const_cast<char **>(&response);
         }
     }
@@ -53,36 +61,44 @@ char ** start_request_1_svc(request *r, struct svc_req *cl)
     return const_cast<char **>(&response);
 }
 
+// Functie RPC prin care clientul transmite argumentul cererii.
 response * send_argument_1_svc(request *r, struct svc_req *cl) {
-    static response res = get_response();
+    static response res = getResponse();
 
     if (!isServerInit) {
-        if (!init_server()) {
+        if (!initServer()) {
             return &res;
         }
     }
 
+    // un client implementat gresit incearca sa trimita un argument fara sa fi
+    // specificat ce fel de operatie vrea sa execute in prealabil
     if (last_request != nullptr) {
         if (strcmp(last_request, SEARCH_REQUEST) == 0) {
             size_t numChars;
             if (isNumber(r->req, &numChars)) {
-                res = get_response("", 0);
+                // cerere SEARCH cu numar
+                res = getResponse("", 0);
                 for (auto & it : word_hits_map) {
                     if (it.first.length() == numChars)
                         res.hits++;
                 }
             } else {
+                // cerere SEARCH cu cuvant
                 int hits = 0;
                 if (word_hits_map.find(r->req) != word_hits_map.end())
                     hits = word_hits_map[r->req];
 
-                res = get_response(r->req, hits);
+                // daca aici as fi folosit word_hits_map[r->req] ca al doilea
+                // argument pentru getResponse, atunci in cazul in care cuvantul
+                // cautat nu exista in map acesta ar fi fost introdus in map
+                res = getResponse(r->req, hits);
             }
         } else if (strcmp(last_request, APPEND_REQUEST) == 0) {
             if (appendFile(r->req))
-                res = get_response(APPEND_SUCCESS, word_hits_map[r->req]);
+                res = getResponse(APPEND_SUCCESS, word_hits_map[r->req]);
             else
-                res = get_response(APPEND_FAIL);
+                res = getResponse(APPEND_FAIL);
         }
     }
 
@@ -94,7 +110,7 @@ bool isNumber(char *str, size_t *outNumber) {
     return std::to_string(*outNumber) == std::string(str);
 }
 
-bool init_server() {
+bool initServer() {
     isServerInit = true;
 
     std::ifstream f(FILENAME);
@@ -112,7 +128,7 @@ bool init_server() {
    return true;
 }
 
-response get_response(char const * respStr, int hits) {
+response getResponse(char const * respStr, int hits) {
     response resp;
     resp.word = const_cast<char *>(respStr);
     resp.hits = hits;
@@ -120,19 +136,18 @@ response get_response(char const * respStr, int hits) {
     return resp;
 }
 
-bool appendFile(char * cPtrWord) {
-    if (cPtrWord == nullptr)
+bool appendFile(char * word) {
+    if (word == nullptr)
         return false;
 
     std::ofstream f(FILENAME, std::ios::app);
     if (!f)
         return false;
 
+    // nu scrie newline pentru primul cuvant din fisier
     if (f.tellp() != 0)
         f << "\n";
-    f << cPtrWord;
-
-    std::string word(cPtrWord);
+    f << word;
 
     if (word_hits_map.find(word) == word_hits_map.end())
         word_hits_map[word] = 0;
